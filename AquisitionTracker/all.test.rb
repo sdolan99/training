0fdd404db500461950f109cba380acc4f6283c2c
Steven Dolan <sdolan@squareup.com>
2015-11-20 14:49:07 -0800

    updating substitute timestamp to take entry rather than list. more tests and readme

---
+++ AquisitionTracker/all.test.rb
@@ -0,0 +1,1 @@
+require_relative 'read_journal.test'
--- AquisitionTracker/read_journal.rb
+++ AquisitionTracker/read_journal.rb
@@ -1,6 +1,47 @@
 #!/usr/bin/env ruby
 require 'time'
 
+# input journal entry output the same
+# mutates journal_entries
+def substitute_real_timestamps!(journal_entry, epoc, increment = 60)
+  journal_entry['timestamp'] = epoc + journal_entry['timestamp'].split('t')[1].to_i * increment
+  # Can change this to just entry['facts'] if needed
+  journal_entry.keys do |fact|
+    fact.map! { |e| e.match(/\A:_t\d+\z/) ? e.split('t')[1].to_i * increment : e }
+  end
+end
+
+def substitute_real_fact_uuids(journal_entries, uuid_map)
+  journal_entries.each do |entry|
+    entry['facts'].each do |fact|
+      fact.map! { |e| e.match(/\A:_/) ? uuid_map[e] : e }
+    end
+  end
+end
+
+def check_timestamps
+  given = [{ 'timestamp' => ':_t1' }]
+  expect = [{ 'timestamp' => Time.parse('2015-11-13 00:01:00 -0800') }]
+  epoc = Time.parse('2015-11-13 00:00:00 -0800')
+  actual = substitute_real_timestamps(given, epoc)
+  puts "check_timestamps: #{expect == actual}"
+  puts "#{expect} != #{actual}" if expect != actual
+end
+
+def check_uuid
+  maplist = { ':_mike' => '45736D07-94F9-4DB8-BAC6-02DC455B3B73' }
+  given = [{ 'facts' => [[':assert', ':_mike', 'person/name', 'Mike']] }]
+  expect = [{ 'facts' => [[':assert', '45736D07-94F9-4DB8-BAC6-02DC455B3B73', 'person/name', 'Mike']] }]
+  actual = substitute_real_fact_uuids(given, maplist)
+  puts "check_uuid: #{expect == actual}"
+  puts "#{expect} != #{actual}" if expect != actual
+end
+
+if ENV['test']
+  check_timestamps
+  check_uuid
+end
+__END__
  maplist = {
   ':_mike': '45736D07-94F9-4DB8-BAC6-02DC455B3B73',
   ':_E3-1231': '5497558D-3A0C-459F-ACA7-381610C359A4',
@@ -14,47 +55,3 @@ require 'time'
   ':_server1': 'B42C6EA7-15FA-4931-9F24-720F89357D62',
   ':_deploy1': 'A7E44B53-2511-4944-8BA1-DB58E940CA89',
 }
-
-# input journal entries output the same
-def substitute_real_timestamps( journal_entries, epoc )
-  increment = 60
-  journal_entries.each do |entry|
-    entry['timestamp'] = epoc + entry['timestamp'].split('t')[1].to_i * increment
-    # Can change this to just entry['facts'] if needed
-    entry.keys do |fact|
-      fact.map! { |e| e.match(/\A:_t\d+\z/) ? e.split('t')[1].to_i * increment : e }
-    end
-  end
-end
-
-def substitute_real_fact_uuids( journal_entries, uuid_map )
-  journal_entries.each do |entry|
-    entry['facts'].each do |fact|
-      fact.map! { |e| e.match(/\A:_/) ? uuid_map[e] : e }
-    end
-  end
-end
-
-
-def check_timestamps
-  given = [ {'timestamp' => ':_t1'}, ]
-  expect = [ {'timestamp' => Time.parse('2015-11-13 00:01:00 -0800')} ]
-  epoc = Time.parse('2015-11-13 00:00:00 -0800')
-  actual = substitute_real_timestamps(given, epoc)
-  puts "check_timestamps: #{expect == actual}"
-  puts "#{expect} != #{actual}" if expect != actual
-end
-
-def check_uuid
-  maplist = { ':_mike' => '45736D07-94F9-4DB8-BAC6-02DC455B3B73' }
-  given = [{'facts'=>[[':assert', ':_mike', 'person/name', 'Mike']]}]
-  expect = [{'facts'=>[[':assert', '45736D07-94F9-4DB8-BAC6-02DC455B3B73', 'person/name', 'Mike']]}]
-  actual = substitute_real_fact_uuids(given, maplist)
-  puts "check_uuid: #{expect == actual}"
-  puts "#{expect} != #{actual}" if expect != actual
-end
-
-if ENV['test']
-  check_timestamps
-  check_uuid
-end
