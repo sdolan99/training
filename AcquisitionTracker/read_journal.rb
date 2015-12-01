9247d319c25a71b94f6e2d5622e52906bea9c42b
Myles Byrne <myles@myles.id.au>
2015-11-30 17:39:04 -0800

    Add namespaces
    This is the annoying (but necessary) code that ensures that these
    methods will not accidentally collide with other methods elsewhere
    in the app that have the same name.
    
    Note the code doesn't change when it gets namespaced, it just gets
    surrounded in more code.
    
    So the un-namespaced method:
    
        def foo
        end
    
    Becomes:
    
        module Namespace
          def foo
          end
        end
    
    This is nice because it means you don't have to think about namespaces
    up-front when sketching something out.
    
    The `module_function` macro is used to indicate that the method is
    actually a pure function and so doesn't need to be "mixed in" to some
    class before it can be used.
    
    Module functions can be called directly on the module, e.g.:
    
        Namespace.foo()

--- AcquisitionTracker/read_journal.rb
+++ AcquisitionTracker/read_journal.rb
@@ -2,37 +2,53 @@ require 'time'
 require 'yaml'
 require 'digest/sha1'
 
-def transform_timestamp(timecode, starting_time, increment)
-  return timecode unless timecode.to_s.match(/\A:_t\d+\z/)
-  starting_time + timecode.split('t')[1].to_i * increment
-end
-
-# input journal entry output the same
-# mutates journal_entries
-def substitute_real_timestamps!(journal_entry, epoc = Time.now, increment = 60)
-  journal_entry['timestamp'] = transform_timestamp(journal_entry['timestamp'], epoc, increment)
-
-  # Can change this to just entry['facts'] if needed
-  journal_entry.keys do |fact|
-    fact.map! { |e| transform_timestamp(e, epoc, increment) }
-  end
-  journal_entry
-end
-
-def substitute_real_fact_uuids!(journal_entry)
-  journal_entry['facts'].each do |fact|
-    fact.map! do |e|
-      e.to_s.match(/\A:_/) ? Digest::SHA1.hexdigest(e).slice(0, 32) : e
+# Application Namespace
+module AquisitionTracker
+  # Functions to transform seed journal entries into "real" ones
+  module ReadJournal
+    # journal_entries -> journal_entries
+    def transform_seed_entries(journal_entries)
+      journal_entries.map do |journal_entry|
+        substitute_real_timestamps!(journal_entry)
+        substitute_real_fact_uuids!(journal_entry)
+      end
     end
+    module_function :transform_seed_entries
+
+    # symbol, time, int -> time
+    def transform_timestamp(timecode, starting_time, increment)
+      return timecode unless timecode.to_s.match(/\A:_t\d+\z/)
+      starting_time + timecode.split('t')[1].to_i * increment
+    end
+    module_function :transform_timestamp
+
+    # journal_entry -> journal_entry
+    def substitute_real_timestamps!(journal_entry, epoc = Time.now, increment = 60)
+      journal_entry['timestamp'] = transform_timestamp(journal_entry['timestamp'], epoc, increment)
+
+      # Can change this to just entry['facts'] if needed
+      journal_entry.keys do |fact|
+        fact.map! { |e| transform_timestamp(e, epoc, increment) }
+      end
+      journal_entry
+    end
+    module_function :substitute_real_timestamps!
+
+    # journal_entry -> journal_entry
+    def substitute_real_fact_uuids!(journal_entry)
+      journal_entry['facts'].each do |fact|
+        fact.map! do |e|
+          e.to_s.match(/\A:_/) ? Digest::SHA1.hexdigest(e).slice(0, 32) : e
+        end
+      end
+      journal_entry
+    end
+    module_function :substitute_real_fact_uuids!
   end
-  journal_entry
 end
 
 if $PROGRAM_NAME == __FILE__
-  mapped_journal_entries = YAML.load_stream($stdin.read).map do |journal_entry|
-    substitute_real_timestamps!(journal_entry)
-    substitute_real_fact_uuids!(journal_entry)
-  end
-
-  puts YAML.dump_stream(*mapped_journal_entries)
+  include AquisitionTracker::ReadJournal
+  # read yaml from stdin, transform, write yaml to stdout
+  puts YAML.dump_stream(*transform_seed_entries(YAML.load_stream($stdin.read)))
 end
--- AcquisitionTracker/read_journal.test.rb
+++ AcquisitionTracker/read_journal.test.rb
@@ -2,6 +2,8 @@ require 'time'
 require 'minitest/autorun'
 require_relative 'read_journal'
 
+include AquisitionTracker::ReadJournal
+
 describe 'substitute_real_timestamps' do
   it 'Works for :t_1' do
     given = { 'timestamp' => ':_t1' }
@@ -20,26 +22,3 @@ describe 'substtute_real_fact_uuids' do
     assert_equal expect, actual
   end
 end
-
-# def check_timestamps
-#   given = [ {'timestamp' => ':_t1'}, ]
-#   expect = [ {'timestamp' => Time.parse('2015-11-13 00:01:00 -0800')} ]
-#   epoc = Time.parse('2015-11-13 00:00:00 -0800')
-#   actual = substitute_real_timestamps(given, epoc)
-#   puts "check_timestamps: #{expect == actual}"
-#   puts "#{expect} != #{actual}" if expect != actual
-# end
-
-# def check_uuid
-#   maplist = { ':_mike' => '45736D07-94F9-4DB8-BAC6-02DC455B3B73' }
-#   given = [{'facts'=>[[':assert', ':_mike', 'person/name', 'Mike']]}]
-#   expect = [{'facts'=>[[':assert', '45736D07-94F9-4DB8-BAC6-02DC455B3B73', 'person/name', 'Mike']]}]
-#   actual = substitute_real_fact_uuids(given, maplist)
-#   puts "check_uuid: #{expect == actual}"
-#   puts "#{expect} != #{actual}" if expect != actual
-# end
-
-# if ENV['test']
-#   check_timestamps
-#   check_uuid
-# end
