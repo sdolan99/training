8c261f81e3cbc1141ee3980176dcbe99efcc15a0
Steven Dolan <sdolan@squareup.com>
2016-03-04 10:33:12 -0800

    view and add existing parts in web

--- AcquisitionTracker/server.rb
+++ AcquisitionTracker/server.rb
@@ -12,13 +12,34 @@ module AcquisitionTracker
       erb:api
     end
 
-    get '/api/v1/inventory-report' do
+    get '/inventory_report/' do
       Commands.hydrate Journal.load_journal_entries
       data = Queries.inventory_report
-      returnstring = StringIO.new
-      Ui.inventory_status_report(data, returnstring)
-      returnstring.string
+      min_quantity = data.key?('min_quantity') ? data['min_quantity'] : 0
+      erb :ir, :locals => { 'data' => data, 'min_quantity' => min_quantity }
     end
 
+    get '/acquire_part/' do
+      Commands.hydrate Journal.load_journal_entries
+      p = Queries.all_parts
+      puts "query: "
+      puts p
+
+      parts_list = Ui::parts_list(Queries.all_parts).map { |l| l.strip }
+      puts "parts are:"
+      puts parts_list
+      erb :add_part_form, :locals => {:parts_list => parts_list}
+    end
+
+    post '/acquire_part/' do
+      user_entry = {}
+      user_entry['existing_part_id'] = params[:existing_part_id]
+      user_entry['date_acquired'] = params[:date_acquired]
+      add_part_entry = Ui::Translate.write_new_add_part_entry(user_entry, Queries.all_parts)
+      puts add_part_entry
+      Journal.write_entry(add_part_entry)
+      Commands.hydrate([add_part_entry])
+      "Added part #{user_entry['existing_part_id']}"
+    end
   end
 end
