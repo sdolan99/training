a01c59346659a0eed8ed140ebbd86aadac42cd4d
Steven Dolan <sdolan@squareup.com>
2016-03-03 22:52:38 -0800

    adding sinatra web

--- AcquisitionTracker/server.rb
+++ AcquisitionTracker/server.rb
@@ -1,9 +1,24 @@
 require 'sinatra'
+require_relative 'main'
 
 module AcquisitionTracker
+  # TODO: Where do we hydrate on startup?
   class AtServer < Sinatra::Base
     get '/' do
-      "Hello world"
+      erb:index
     end
+
+    get '/api' do
+      erb:api
+    end
+
+    get '/api/v1/inventory-report' do
+      Commands.hydrate Journal.load_journal_entries
+      data = Queries.inventory_report
+      returnstring = StringIO.new
+      Ui.inventory_status_report(data, returnstring)
+      returnstring.string
+    end
+
   end
 end
